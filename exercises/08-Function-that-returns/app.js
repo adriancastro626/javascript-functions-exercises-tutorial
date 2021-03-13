757c3c4e5758500d97e2bf7fbbef3e27b5067990
adriancastro626 <adriancastro626@icloud.com>
2021-03-13 16:32:22 +0000

    done

--- exercises/08-Function-that-returns/app.js
+++ exercises/08-Function-that-returns/app.js
@@ -1,8 +1,13 @@
 var dollarToEuro = function(dollarValue){
-	return dollarValue * 0.89;
+	return dollarValue * 0.89; // 121.93 euros
 }
 var euroToYen = function(euroValue){
-	return euroValue * 124.15;
+	return euroValue * 124.15;  // 15137.60 yenes
 }
 //***** YOUR CODE BELOW ↓ ******///
+
+let euros = dollarToEuro(137);
+let yenes = euroToYen(euros);
+
+console.log(yenes);
 
--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -2,5 +2,19 @@
 
 
 
-  ///*** Do not edit below this line ****/
-  console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
+function renderPerson(a, b, c, d, e) {
+
+a = "Bob is a ";
+b = "23 ";
+c = "years old male born in ";
+d = "05/22/1983 ";
+e = "with green eyes";
+
+    return a + b + c + d + e;
+
+
+
+}
+
+///*** Do not edit below this line ****/
+console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
